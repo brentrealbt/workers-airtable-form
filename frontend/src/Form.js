1ff928ee038be61fa0a7b44f12310676e5710ee5
Brent Thomas <brent@bt.associates>
2021-12-12 22:13:07 -0500

    Update the boxes to blue

--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -14,7 +14,7 @@ export default () => (
           autoComplete="given-name"
           placeholder="Ellen"
           required
-          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-500 focus:border-blue-500 border-warm-gray-300 rounded-md"
         />
       </div>
     </div>
@@ -30,7 +30,7 @@ export default () => (
           autoComplete="family-name"
           placeholder="Ripley"
           required
-          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+          className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-blue-500 focus:border-blue-500 border-warm-gray-300 rounded-md"
         />
       </div>
     </div>
@@ -111,7 +111,7 @@ export default () => (
     <div className="sm:col-span-2 sm:flex sm:justify-end">
       <button
         type="submit"
-        className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-teal-500 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:w-auto"
+        className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:w-auto"
       >
         Submit
       </button>
