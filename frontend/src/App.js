ea392474889ac09141ac9367b7a18fa9617d232e
Brent Thomas <brent@bt.associates>
2021-12-12 20:34:16 -0500

    Change title and opening detail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,13 +40,10 @@ export default function FormPage() {
           <div className="py-24 lg:py-32">
             <div className="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
               <h1 className="text-4xl font-extrabold tracking-tight text-warm-gray-900 sm:text-5xl lg:text-6xl">
-                Workers Airtable Form Example
+                Cybersecurity Mental Health - Survey
               </h1>
               <p className="mt-6 text-xl text-warm-gray-500 max-w-3xl">
-                An example form that uses serverless functions, provided by <ExternalLink href="https://workers.dev">Cloudflare Workers</ExternalLink>, to submit form data to an <ExternalLink href="https://airtable.com">Airtable</ExternalLink> table.{" "}
-                <ExternalLink href={REPO_URL}>
-                  Check out the source to learn more.
-                </ExternalLink>
+                A survey to collect your thoughts on mental health amongst the cybersecurity workforce.
               </p>
             </div>
           </div>
