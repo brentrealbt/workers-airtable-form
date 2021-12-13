0758e61c999d23014593416dbc6460a7a25ccbc5
Brent Thomas <brent@bt.associates>
2021-12-12 21:18:36 -0500

    Change text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,12 +39,12 @@ export default function FormPage() {
         <div className="bg-warm-gray-50">
           <div className="py-24 lg:py-32">
             <div className="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
-              <h1 className="text-4xl font-extrabold tracking-tight text-warm-gray-900 sm:text-5xl lg:text-6xl">
-                Cybersecurity Mental Health - Survey
-              </h1>
-              <p className="mt-6 text-xl text-warm-gray-500 max-w-3xl">
-                A survey to collect your thoughts on mental health amongst the cybersecurity workforce.
-              </p>
+              <h2 className="text-4xl font-extrabold tracking-tight text-warm-gray-900 sm:text-5xl lg:text-6xl">
+                Cybersecurity Mental Health
+              </h2>
+              <h3 className="mt-6 text-xl text-warm-gray-500 max-w-3xl">
+                A Workforce Survey
+              </h3>
             </div>
           </div>
         </div>
